1dbd4034b2449964e156722336a89842b22abb4e
VazelinInside <vi4733425@gmail.com>
2024-02-19 15:32:57 +0300

    work on mistakes-version 1

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
     })
+    document.getElementById("menu").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
 })
